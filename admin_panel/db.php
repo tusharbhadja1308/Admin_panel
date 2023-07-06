5d00e0da867076cc3ce842c989fffe930e50a6a7
tusharbhadja1308 <138601239+tusharbhadja1308@users.noreply.github.com>
2023-07-06 15:17:41 +0530

    Delete admin_panel directory

--- admin_panel/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "admin", "Admin@123", "login_system");
-// $conn = mysqli_connect("localhost", "root", "", "login_system");
-
-if (!$conn) {
-    echo mysqli_connect_errno();
-}
